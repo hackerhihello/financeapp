d4565acf89c40d19c48047a7935b8be7fdadc0eb
hackerhihello <hackerhihello123@gmail.com>
2024-02-22 10:53:58 +0530

    Your commit message here

--- server.js
+++ server.js
@@ -14,14 +14,18 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
+require('dotenv').config();
 
+
+uri=process.env.MONGODB_URI;
+console.log('Connecting to MongoDB at:', uri);
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/financeApp', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error(err));
 
   app.get('/',  async (req, res) => { 
-  res.json("hello world");
+  res.send("hello world");
   })
 
 // Routes
